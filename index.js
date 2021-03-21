eb6f0c323d28a232d8f7b614448baec9c1ae43a2
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-21 15:41:12 +0100

    setup dotenv and configured mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
 const typeDefs = gql`
   type Query {
@@ -15,6 +16,15 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server started at ${res.url}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://Nave18:${process.env.MONGODB}@devsocialnetwork.9klrg.mongodb.net/GraphqlNetwork?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server started at ${res.url}`);
+  });
