08418285584b62ffd6da0d349c29e8cae67110bd
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-22 12:49:44 +0100

    added user and post models

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require(".models/Post.js");
+
 const typeDefs = gql`
   type Query {
     sayHi: String!
