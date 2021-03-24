85292192a5e1047657aa6bbe94d2fc15fb354e0a
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-24 22:52:19 +0100

    fixed User model import error

--- src/graphql/resolvers/users.js
+++ src/graphql/resolvers/users.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import User from "../../models/User";
+import { User } from "../../models/User";
 
 const usersResolvers = {
   Mutation: {
